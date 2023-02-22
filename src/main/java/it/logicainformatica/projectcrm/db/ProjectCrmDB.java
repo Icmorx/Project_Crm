6d4ec1c79ac07e583703d15e903b02062c1f3e59
Mirco <mircolaudazi@gmail.com>
2023-02-22 15:56:45 +0100

    fix

--- src/main/java/it/logicainformatica/projectcrm/db/ProjectCrmDB.java
+++ src/main/java/it/logicainformatica/projectcrm/db/ProjectCrmDB.java
@@ -13,7 +13,7 @@ import it.logicainformatica.projectcrm.bean.ProjectCrmBean;
 public class ProjectCrmDB {
 
 	DataBase db = new DataBase();
-	
+
 	// INSERIRE I DATI SUL DB
 	public void inserisciUtente(ProjectCrmBean p) {
 
@@ -26,14 +26,14 @@ public class ProjectCrmDB {
 			dbconn = db.getConnessione();
 
 			// PREPARO L'ISTRUZIONE SQL
-			PreparedStatement statement = dbconn.prepareStatement(
-					"INSERT INTO anagrafica(nome, cognome, telefono) VALUES(?, ?, ?)");
+			PreparedStatement statement = dbconn
+					.prepareStatement("INSERT INTO anagrafica(nome, cognome, telefono) VALUES(?, ?, ?)");
 
 			// SOSTITUISCO IL ? CON UN VALORE REALE
 			statement.setString(1, p.getNome());
 			statement.setString(2, p.getCognome());
 			statement.setString(3, p.getTelefono());
-			
+
 			// ESEGUO LA QUERY SUL DB
 			statement.execute();
 
@@ -48,15 +48,15 @@ public class ProjectCrmDB {
 		}
 
 	}
-	
+
 	// STAMPA I DATI
 	public void stampaDati() {
 
 		// CREO L'OGGETTO CONNECTION
 		Connection dbconn = null;
-		
+
 		try {
-			
+
 			FileWriter scrittura = new FileWriter("Project_Crm.txt");
 
 			// VALORIZZO L'OGGETTO CONNECTION
@@ -74,19 +74,20 @@ public class ProjectCrmDB {
 
 				// IMPORTO CLASSE BEAN
 				ProjectCrmBean p = new ProjectCrmBean();
-				
+
 				// INSERENDO IL NOME DELLA COLONNA E MI PRENDO IL VALORE
 				p.setId(rs.getInt("id"));
-				 p.setNome(rs.getString("nome"));
-				 p.setCognome(rs.getNString("cognome"));
-				 p.setTelefono(rs.getString("telefono"));
-				 
-				 scrittura.write("Id: " + p.getId() + ",");
-				 scrittura.write("Nome: " + p.getNome() + ",");
-				 scrittura.write("Cognome: " + p.getCognome() + ",");
-				 scrittura.write("Telefono: " + p.getTelefono() + ",");
-				 
-				 scrittura.close();
+				p.setNome(rs.getString("nome"));
+				p.setCognome(rs.getNString("cognome"));
+				p.setTelefono(rs.getString("telefono"));
+
+				scrittura.write("Id: " + p.getId() + ",");
+				scrittura.write("Nome: " + p.getNome() + ",");
+				scrittura.write("Cognome: " + p.getCognome() + ",");
+				scrittura.write("Telefono: " + p.getTelefono() + ",");
+ 
+				// CHIUSRA WRITER
+				scrittura.close();
 			}
 
 		} catch (SQLException e) {
